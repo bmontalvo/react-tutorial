47ac856be2457510826f06ab0ca89768bccfad08
Bobby Montalvo <bmontalvo@movietickets.com>
2016-09-06 18:04:15 -0400

    Working. Finished the 'Adding Markdown' section of tutorial.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 // src/components/App/index.js
 import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames';
+import Remarkable from 'remarkable';
 
 import logo from './logo.svg';
 import './style.css';
@@ -35,7 +36,65 @@ export class CommentBox extends Component {
   render() {
     return (
       <div className={classnames('CommentBox', this.props.className)} style={this.props.style}>
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+}
+
+export class CommentList extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+  }
+
+  render() {
+    return (
+      <div className={classnames('CommentList', this.props.className)} style={this.props.style}>
+        <Comment author="Mike Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+}
+
+export class CommentForm extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+  }
+
+  render() {
+    return (
+      <div className={classnames('CommentForm', this.props.className)} style={this.props.style}>
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
+
+export class Comment extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+  }
+
+  rawMarkup() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  }
+
+  render() {
+    var md = new Remarkable();
+    return (
+      <div className={classnames('Comment', this.props.className)} style={this.props.style}>
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
